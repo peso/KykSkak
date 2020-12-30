49370293fd54e0ece880747fa614a52190e90a84
Peer Sommerlund <peer.sommerlund@gmail.com>
2020-12-30 23:53:19 +0100

    Make clock count down

---
+++ Clock.gd
@@ -0,0 +1,26 @@
+extends TextureButton
+
+
+# Declare member variables
+export var time_left = 300
+export var running = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if running:
+		time_left -= delta
+		
+		# Make arms shake when all time is used
+		if time_left < -1:
+			time_left += 1
+	
+	# Show mm:ss as hh:mm:00
+	# Clock should work backwards
+	$Min.rotation_degrees = time_left * 360 / 60
+	$Hour.rotation_degrees = time_left * 360 / (60 * 12)
